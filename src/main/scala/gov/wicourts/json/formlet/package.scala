c51d368e1e6131564d7d533f3df1b76b93818b6b
Ivan Jager <ivan.jager@wicourts.gov>
2016-02-09 13:18:56 -0600

    Redefine the Formlet type aliases to make the relation between them clearer

--- src/main/scala/gov/wicourts/json/formlet/package.scala
+++ src/main/scala/gov/wicourts/json/formlet/package.scala
@@ -9,13 +9,15 @@ import scala.language.higherKinds
 import scalaz.NonEmptyList
 
 package object formlet {
-  type IdFieldFormlet[A] = Formlet[Id, Option[Cursor], NonEmptyList[String], A, FieldView]
-  type IdObjectFormlet[A] = Formlet[Id, Option[Cursor], ValidationErrors, A, JsonObjectBuilder]
-
-  type FieldFormlet[M[_], A] = Formlet[M, Option[Cursor], NonEmptyList[String], A, FieldView]
-  type ObjectFormlet[M[_], A] = Formlet[M, Option[Cursor], ValidationErrors, A, JsonObjectBuilder]
 
   type JsonFormlet[M[_], E, A, V] = Formlet[M, Option[Cursor], E, A, V]
+
+  type FieldFormlet[M[_], A] = JsonFormlet[M, NonEmptyList[String], A, FieldView]
+  type ObjectFormlet[M[_], A] = JsonFormlet[M, ValidationErrors, A, JsonObjectBuilder]
+
+  type IdFieldFormlet[A] = FieldFormlet[Id, A]
+  type IdObjectFormlet[A] = ObjectFormlet[Id, A]
+
 
   object syntax extends ToFieldFormletOps with ToObjectFormletOps
 }
