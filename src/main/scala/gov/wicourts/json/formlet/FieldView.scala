242e8c3d0357617ac7b538efe1339342f5dcaaf7
Ivan Jager <ivan.jager@wicourts.gov>
2016-02-12 12:46:33 -0600

    Bring back the scalaz version.
    In anticipation that it will eventually be useful.

--- src/main/scala/gov/wicourts/json/formlet/FieldView.scala
+++ src/main/scala/gov/wicourts/json/formlet/FieldView.scala
@@ -2,12 +2,23 @@ package gov.wicourts.json.formlet
 
 import argonaut.Argonaut.jString
 import argonaut.Json
+
+import scalaz.std.list._
+import scalaz.std.option._
+import scalaz.syntax.monad._
+import scalaz.syntax.std.option._
+
 import scalaz.{@>, Lens}
 
 case class FieldView(name: String, value: Option[Json], label: Option[String], errorName: String) {
   def obj: JsonObjectBuilder = {
-    val metadata = label.map(l =>
-      "metadata" -> Json.obj("label" -> jString(l))
+    val metadataItems =
+      List(
+        label.map(l => ("label", jString(l)))
+      ).map(_.toList).join
+
+    val metadata = metadataItems.headOption.as(
+      ("metadata", Json.obj(metadataItems: _*))
     ).toList
 
     val valueItem = value.map(("value", _)).toList
